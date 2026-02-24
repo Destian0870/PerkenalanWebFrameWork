321cb7a5d939cdb4c617b85b8732e5f4f495910c
Destian0870 <destian0870@gmail.com>
2026-02-24 21:24:09 +0700

    Menyimpan Semua Perubahan yang sudah dilakukan

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function hello() {
+ return 'Hello World';
+}
+
+    public function home() {
+return 'Selamat Datang';
+}
+
+    public function about() {
+return 'NIM: 244107020203 - Nama: Destian';
+}
+
+    public function article($id) {
+return 'Halaman Artikel dengan ID ' . $id;
+}
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PhotoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [WelcomeController::class, 'home']);
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+Route::get('/world', function () {
+    return 'World';
 });
+Route::get('/Welcome', function () {
+    return 'Selamat Datang';
+});
+Route::get('/about', [WelcomeController::class, 'about']);
+Route::get('/user/{Destian}', function ($name) {
+    return 'Nama saya '.$name;
+});
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId.' Komentar ke-'.$commentId;
+});
+Route::get('/articles/{id}', [WelcomeController::class, 'article']);
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
+Route::resource('photos', PhotoController::class)->only([
+    'index',
+    'show'
+]);
